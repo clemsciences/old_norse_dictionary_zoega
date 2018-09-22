ece33c458743f1883b2c234f42095682bf822285
clemsciences <clemsciences@aol.com>
2018-09-23 00:03:12 +0200

    Levenshtein distance for approximate search in dictionary

--- reader.py
+++ reader.py
@@ -5,10 +5,25 @@ Code to analyze dictionary entries
 import os
 import codecs
 import re
+
 from xml.etree import ElementTree
 from xml.etree.ElementTree import XMLParser
 
+from cltk.phonology import utils as phu
+from cltk.phonology.old_norse import transcription as ont
+from cltk.phonology.syllabify import Syllabifier
+from cltk.corpus.old_norse.syllabifier import hierarchy, invalid_onsets
+from cltk.text_reuse.levenshtein import Levenshtein
+
 from constants import dheads, postags, dictionary_name
+
+# phonetic transcriber
+phonetic_transcriber = phu.Transcriber(ont.DIPHTHONGS_IPA, ont.DIPHTHONGS_IPA_class, ont.IPA_class, ont.old_norse_rules)
+
+# Old Norse syllabifier
+s = Syllabifier(language="old_norse", break_geminants=True)
+s.set_invalid_onsets(invalid_onsets)
+s.set_hierarchy(hierarchy)
 
 
 def clean(text):
@@ -37,6 +52,16 @@ class Dictionary:
             for entry in self.entries:
                 if entry.word == word:
                     return entry
+
+    def find_approximately(self, word, distance_threshold=3):
+        entries = []
+        if len(self.entries) == 0:
+            self.get_entries()
+        if len(word) > 0:
+            for entry in self.entries:
+                if entry.levenshtein_distance(word) < distance_threshold:
+                    entries.append(entry)
+        return entries
 
 
 class DictionaryDSL:
@@ -94,9 +119,14 @@ class Entry:
         self.pos = [postags[pos.text] for pos in entry_xml.iter("p")]
         self.declensions = []
         self.definition = []
+        self.phonetic_transcription = phonetic_transcriber.main(self.word)
+        self.syllabified_word = s.syllabify_SSP(self.word.lower())
 
     def extract_pos(self):
         pass
+
+    def levenshtein_distance(self, other_word):
+        return Levenshtein.Levenshtein_Distance(self.word, other_word)
 
 
 if __name__ == "__main__":
@@ -106,3 +136,5 @@ if __name__ == "__main__":
     d = Dictionary(dictionary_name)
     print(d.find("sær").description)
     print(d.find("sær").pos)
+    print(d.find("sær").phonetic_transcription)
+    print(d.find("sær").syllabified_word)
